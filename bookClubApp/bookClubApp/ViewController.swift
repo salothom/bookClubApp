2e2e53ced07811b61e4b03c79c6ee5659975ab5e
Sarah Thompson <sarahloisthompson@gmail.com>
2019-11-25 12:30:33 -0600

    Project creation

---
+++ bookClubApp/bookClubApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bookClubApp
+//
+//  Created by Sarah Thompson on 11/25/19.
+//  Copyright © 2019 SarahThompson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
