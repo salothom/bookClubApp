9402b9f46bc40a93b3eae39e65be3273b2ec0dc0
Sarah Thompson <sarahloisthompson@gmail.com>
2019-12-13 11:24:33 -0600

    Making pagre assets

--- bookClubApp/bookClubApp/ViewController.swift
+++ bookClubApp/bookClubApp/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //main
+    
+    @IBAction func DateMeeting(_ sender: UIDatePicker) {
+    }
+    
+    @IBOutlet weak var Genre: UIPickerView!
+    
+    var pickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Connect data:
+        self.Genre.delegate = (self as! UIPickerViewDelegate)
+        self.Genre.dataSource = (self as! UIPickerViewDataSource)
+        
+        // Input the data into the array
+        pickerData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
     }
-
 
 }
 
